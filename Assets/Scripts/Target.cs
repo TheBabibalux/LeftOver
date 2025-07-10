51f09aecb1593c6bfc4ba1f162a229178383a1b6
FélixC <felixludogram@gmail.com>
2025-07-10 18:32:11 +0200

    Setup

---
+++ Assets/Scripts/Target.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Target : MonoBehaviour
+{
+    public Transform targetHead;
+    public Transform targetCenter;
+    public Transform targetLeg;
+}
